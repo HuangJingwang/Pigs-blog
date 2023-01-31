d33bcdefaa6dfb76990239fb0b694d084c49ae07
jingwang.huang <1834191315@qq.com>
2023-01-31 23:25:18 +0800

    fix bug

--- server/src/main/java/com/pigs/blog/common/ErrorCodeEnum.java
+++ server/src/main/java/com/pigs/blog/common/ErrorCodeEnum.java
@@ -2,7 +2,9 @@ package com.pigs.blog.common;
 
 public enum ErrorCodeEnum {
     SERVER_ERROR(500, "there is a error in server"),
-    PARAMETER_IS_ILLEGAL(10001, "parameter is illegal");
+    PARAMETER_IS_ILLEGAL(10001, "parameter is illegal"),
+    PARENT_ID_IS_NOT_EXIST(10002, "parent id is not exist"),
+    PARENT_ID_IS_CONFLICT_TO_CURRENT_ID(10003, "parent id is conflict to current id");
     private Integer code;
     private String msg;
     ErrorCodeEnum(Integer code, String msg){
